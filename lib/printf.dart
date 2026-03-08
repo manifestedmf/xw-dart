edeb29155f01c8c92c35340cd22a488d763fc66c
manifestedmf <misusedinfluenceee@gmail.com>
2026-03-08 18:42:32 +0100

    gonna make priv scanf to be a class instead of a function

--- lib/printf.dart
+++ lib/printf.dart
@@ -6,7 +6,10 @@ import 'src/standard.dart';
 
 /// Prints out to to the console with [stdout]`.`[write]`()`.
 ///
-/// send in the input from [input], and the objects/nulls from [items].
+/// Send in the input from [input], and the objects/nulls from [items].
+///
+/// Adding some digits to the end of a `%?`
+/// will make it try to end at that length, else it will just add it.
 ///
 /// `%o`, grabs the next [Object], which cannot be a [nullable].
 /// (`v2.7.3`)
@@ -24,23 +27,47 @@ void printf<E>(String input, [Iterable<E> items = const []]) {
 
 /// Added in `2.7.3`.
 enum _State {
-  //
+  /// `2.7.3`
   percentage,
+
+  /// `2.7.3`
   object,
+
+  /// `2.7.3`
   nullable,
+
+  /// `2.8`
   iterable,
+
+  /// `2.7.3`
   text,
+
+  /// `2.7.3`
   unknown,
 }
 
 /// Added in `2.7.3`.
 enum _Char {
-  //
+  /// `2.7.3`
   percent,
+
+  /// `2.7.3`
   o,
+
+  /// `2.7.3`
   n,
+
+  /// `2.8`
   a,
+
+  /// `2.7.3`
   text,
+
+  /// `2.8`
+  digit,
+
+  /// `2.8`
+  newline,
 }
 
 /// Added in `2.7.3`.
@@ -49,12 +76,26 @@ _Char _character(String char) => switch (char) {
   "o" => _Char.o,
   "n" => _Char.n,
   "a" => _Char.a,
+  "0" => _Char.digit,
+  "1" => _Char.digit,
+  "2" => _Char.digit,
+  "3" => _Char.digit,
+  "4" => _Char.digit,
+  "5" => _Char.digit,
+  "6" => _Char.digit,
+  "7" => _Char.digit,
+  "8" => _Char.digit,
+  "9" => _Char.digit,
+  "\n" => _Char.newline,
   String() => _Char.text,
 };
 
 /// Returns the formatted [String] from the according [input] and [items].
 ///
-/// send in the input from [input], and the objects/nulls from [items].
+/// Send in the input from [input], and the objects/nulls from [items].
+///
+/// Adding some digits to the end of a `%?`
+/// will make it try to end at that length, else it will just add it.
 ///
 /// `%o`, grabs the next [Object], which cannot be a [nullable].
 /// (`v2.8`)
@@ -105,6 +146,15 @@ String _scanfSafe<E extends Object>(String input, Iterable<E> items) {
 
   /// the value that we are trying to inspect
   E heldValue;
+
+  /// The amount of spaces this currently needs.
+  int spaceAmount = 0;
+
+  /// Current String
+  String tempStr;
+
+  /// current length of line
+  int lineLength = 0;
   while (pointer < input.length) {
     char = input[pointer];
     currentChar = _character(char);
@@ -116,16 +166,20 @@ String _scanfSafe<E extends Object>(String input, Iterable<E> items) {
           case _State.object:
           case _State.nullable:
             heldValue = objects.first;
-            output += heldValue.toString();
+            tempStr = heldValue.toString();
             objects.removeAt(0);
             iterableOffset = cut(val: iterableOffset, max: objects.length - 1);
             state = _State.percentage;
             starter = pointer;
+            output += tempStr;
+            lineLength += tempStr.length;
           case _State.iterable:
             while (true) {
               heldValue = objects[iterableOffset];
               if (heldValue is Iterable) {
-                output += heldValue.toString();
+                tempStr = heldValue.toString();
+                output += tempStr;
+                lineLength += tempStr.length;
                 objects.removeAt(iterableOffset);
                 break;
               } else {
@@ -135,7 +189,9 @@ String _scanfSafe<E extends Object>(String input, Iterable<E> items) {
             state = _State.percentage;
             starter = pointer;
           case _State.text:
-            output += input.substring(starter, pointer);
+            tempStr = input.substring(starter, pointer);
+            output += tempStr;
+            lineLength += tempStr.length;
             state = _State.percentage;
             starter = pointer;
           case _State.unknown:
@@ -145,11 +201,14 @@ String _scanfSafe<E extends Object>(String input, Iterable<E> items) {
       case _Char.o:
         switch (state) {
           case _State.percentage:
+            spaceAmount = 0;
             state = _State.object;
           case _State.object:
           case _State.nullable:
             heldValue = objects.first;
-            output += heldValue.toString();
+            tempStr = heldValue.toString();
+            output += tempStr;
+            lineLength += tempStr.length;
             objects.removeAt(0);
             iterableOffset = cut(val: iterableOffset, max: objects.length - 1);
             state = _State.text;
@@ -158,7 +217,9 @@ String _scanfSafe<E extends Object>(String input, Iterable<E> items) {
             while (true) {
               heldValue = objects[iterableOffset];
               if (heldValue is Iterable) {
-                output += heldValue.toString();
+                tempStr = heldValue.toString();
+                output += tempStr;
+                lineLength += tempStr.length;
                 objects.removeAt(iterableOffset);
                 break;
               } else {
@@ -175,6 +236,7 @@ String _scanfSafe<E extends Object>(String input, Iterable<E> items) {
       case _Char.n:
         switch (state) {
           case _State.percentage:
+            spaceAmount = 0;
             state = _State.nullable;
           case _State.object:
           case _State.nullable:
@@ -205,6 +267,7 @@ String _scanfSafe<E extends Object>(String input, Iterable<E> items) {
       case _Char.a:
         switch (state) {
           case _State.percentage:
+            spaceAmount = 0;
             state = _State.iterable;
           case _State.object:
           case _State.nullable:
@@ -262,6 +325,51 @@ String _scanfSafe<E extends Object>(String input, Iterable<E> items) {
           case _State.unknown:
             state = _State.text;
         }
+      case _Char.digit:
+        switch (state) {
+          case _State.percentage:
+            state = _State.text;
+          case _State.object:
+          case _State.nullable:
+          case _State.iterable:
+            spaceAmount = spaceAmount * 10 + int.parse(char);
+          case _State.text:
+            state = _State.text;
+          case _State.unknown:
+            state = _State.text;
+        }
+      case _Char.newline:
+        switch (state) {
+          case _State.percentage:
+            break;
+          case _State.object:
+          case _State.nullable:
+            heldValue = objects.first;
+            output += heldValue.toString();
+            objects.removeAt(0);
+            iterableOffset = cut(val: iterableOffset, max: objects.length - 1);
+            state = _State.text;
+            starter = pointer;
+          case _State.iterable:
+            while (true) {
+              heldValue = objects[iterableOffset];
+              if (heldValue is Iterable) {
+                tempStr = heldValue.toString();
+                output += tempStr;
+                objects.removeAt(iterableOffset);
+                break;
+              } else {
+                iterableOffset++;
+              }
+            }
+            state = _State.text;
+            starter = pointer;
+          case _State.text:
+            break;
+          case _State.unknown:
+            state = _State.text;
+        }
+        lineLength = 0;
     }
     pointer++; // increments
   }
